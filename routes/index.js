5138d320fff8d1d3080e011cbc89c3c8f8e6f323
Frijol <ifoundthemeaningoflife@gmail.com>
2014-06-04 21:00:58 -0700

    setups for both celery and rush order in place, both failing

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,37 @@
-var secrets = require('./secrets');
+// Module dependencies
+var request = require('request');
+var rushorder = require('rushorder');
 
-exports.index = function(req, res){
+// Access tokens etc
+var secrets = require('../secrets.js').secrets,
+  celery = secrets.celery,
+  ro = secrets.ro,
+  plotly = secrets.plotly;
+
+exports.index = function(req, res) {
+  // Get data from Celery
+  // getCelery(celery);
+  // Get data from Rush Order
+  getRushOrder(ro);
+  // Graph with Plotly
+  // Render the page
   res.render('index', { title: 'Express' });
 };
 
-// function
+function getCelery (celery) {
+  request('https://api.trycelery.com/v1/orders?access_token=' + celery.token, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body);
+    } else {
+      console.log(error, response.statusCode);
+    }
+  });
+}
+
+function getRushOrder (ro) {
+  rushorder.getAPIKey(ro.username, ro.password, function (err, key) {
+    rushorder.getInventory(key, function (err, inventory) {
+      console.log(inventory);
+    });
+  });
+}
