0cf0026bba202befd71d1db029e46104b1089f2f
Frijol <ifoundthemeaningoflife@gmail.com>
2014-06-05 22:29:53 -0700

    parsing very basic stats on orders

--- routes/index.js
+++ routes/index.js
@@ -8,23 +8,62 @@ var secrets = require('../secrets.js').secrets,
   ro = secrets.ro,
   plotly = secrets.plotly;
 
-exports.index = function(req, res) {
+exports.index = function (req, res) {
   // Get data from Celery
   getCelery(celery, function (err, celeryData) {
+    var ordersRaw = celeryData.orders;
+    var total = celeryData.total;
+    sortByStatus(ordersRaw, function (orders) {
+      var paid = orders.paid;
+      var open = orders.open;
+      var failed = orders.failed;
+      var cancelled = orders.cancelled;
+      res.render('index', { title: 'Dashboard',
+        total: total,
+        numPaid: paid.length,
+        numOpen: open.length,
+        numFailed: failed.length,
+        numCancelled: cancelled.length
+      });
+    });
     // Get data from Rush Order
     // getRushOrder(ro);
     // Graph with Plotly
     // Render the page
-    res.render('index', { title: 'Dashboard', celeryData: celeryData });
   });
 };
 
 function getCelery (celery, callback) {
   request('https://api.trycelery.com/v1/orders?access_token=' + celery.token, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      callback(null, body);
+      callback(null, JSON.parse(body));
     } else {
       callback([error, response.statusCode]);
+    }
+  });
+}
+
+function sortByStatus (orders, callback) {
+  var paid = [];
+  var open = [];
+  var failed = [];
+  var cancelled = [];
+  orders.forEach(function (order) {
+    // Sort
+    if (order.status === 'paid_balance') {
+      paid.push(order);
+    } else if (order.status === 'open') {
+      open.push(order);
+    } else if (order.status === 'cancelled' || order.status === 'refunded_balance') {
+      cancelled.push(order);
+    } else if (order.status === 'charge_balance_failed') {
+      failed.push(order);
+    } else {
+      console.log(order); // in case we're missing anything
+    }
+    // Callback
+    if((paid.length + open.length + failed.length + cancelled.length) == orders.length) {
+      callback({paid: paid, open: open, failed: failed, cancelled: cancelled});
     }
   });
 }
